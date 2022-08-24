b697d41ba0b3965a9c96b36d5c055436fd74c4ef
Serhii Svystunov <snsvistunov@gmail.com>
2022-08-24 12:51:31 +0300

    Minor fixes

--- go-user-service/controllers/profile.go
+++ go-user-service/controllers/profile.go
@@ -8,8 +8,6 @@ import (
 )
 
 func GetUserProfile(context *gin.Context) {
-	//var user models.User
-
 	username := context.Request.Header["Username"]
 	if user, ok := data.Users[username[0]]; ok {
 		context.SecureJSON(http.StatusOK, gin.H{"UserProfile": user})
